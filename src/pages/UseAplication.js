e8772be6c451997e9d295dcabde4d1de7219a939
AliCuchalloGabrielFernando <f1.gabriel73@gmail.com>
2023-06-09 17:15:11 -0400

    correjido error con formato png

--- src/pages/UseAplication.js
+++ src/pages/UseAplication.js
@@ -24,27 +24,27 @@ export default function UserAplication(){
             Ingresa a tu cuenta de Google a la seccion Datos y Privacidad <br></br>
             Busca el Apartado Descargar o Eliminar tus datos <br></br>
             Selecciona Descargar tus datos <br></br>
-        <img src="paso1.png"  className="h-96 w-full rounded-lg"alt="nature image"/>
+        <img src="paso1.png"  className="h-96 w-full rounded-lg"alt="paso 1"/>
 
         </Typography>
         <br></br>
         <Typography variant="h5">Paso 2 </Typography>
         <Typography variant="paragraph">
             Desmarca todos los productos
-            <img src="paso2.png"  className="h-64 w-96 rounded-lg"alt="nature image"/>
+            <img src="paso2.png"  className="h-64 w-96 rounded-lg"alt="paso 2"/>
             Selecciona unicamente Google Chrome y selecciona "Se han incluido todos datos de Chrome" <br></br>
             Para desmarca todas las opciones y marca solamente BrowserHistory
-            <img src="paso3.png"  className="h-64 w-96 rounded-lg"alt="nature image"/>
+            <img src="paso3.png"  className="h-64 w-96 rounded-lg"alt="paso 3"/>
             Luego desciende al final de la lista de los productos seleccionados y da click a "Siguiente Paso"
         </Typography>
         <br></br>
         <Typography variant="h5">Paso 3 </Typography>
         <Typography variant="paragraph">
             Esta debe ser su configuracion para descargar solo una vez.
-            <img src="paso4.png"  className="h-96 w-96 rounded-lg"alt="nature image"/>
+            <img src="paso4.png"  className="h-96 w-96 rounded-lg"alt="paso 4"/>
             Revisa tu Bandeja de entrada en Gmail y descarga el archivo comprimido <br></br>
             Entra  a la carpeta Takeout/Chrome y ahi esta tu archivo BrowserHistory.json
-        <img src="paso5.png"  className="h-96 w-full rounded-lg"alt="nature image"/>
+        <img src="paso5.png"  className="h-96 w-full rounded-lg"alt="paso 5"/>
         </Typography>
         <br></br>
         <Typography variant="h5">Paso 4 </Typography>
@@ -52,7 +52,7 @@ export default function UserAplication(){
             La unica informacion que requiere esta aplicacion es el campo "url"<br></br>
             Puedes eliminar los datos de los otros campos, pero manten la estructura del archivo <br></br>
             Recordar que esta pagina solo es un trabajo aplicativo de alto procesamiento de datos aplicados a MongoDB
-            <img src="paso6.png"  className="h-96 w-full rounded-lg"alt="nature image"/>
+            <img src="paso6.png"  className="h-96 w-full rounded-lg"alt="paso 6"/>
         </Typography>
       </div>
       </div>
